8e879e801624bbeb49b1efbfa2541f225b67b97c
Monica <mntamaru@gmail.com>
2018-05-31 10:47:46 -0700

    added db table creations

---
+++ db/migrate/02_create_logs.rb
@@ -0,0 +1,11 @@
+class CreateLog < ActiveRecord::Migration
+	def change
+		create_table :logs do |t|
+			t.string :date
+			t.integer :swim_distance
+			t.integer :bike_distance
+			t.integer :run_distance
+			t.integer :user_id
+		end
+	end
+end
